19e69e9377d7bc6f4a63f1d66eb14b347d794187
vitalir2 <vitalyi100@mail.ru>
2022-02-22 19:52:24 +0300

    Refactor and add some new tests in the 3rd task

--- src/test/java/task3/Task3Test.java
+++ src/test/java/task3/Task3Test.java
@@ -7,12 +7,14 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 // variant 13
 public class Task3Test {
 
+    private static final int SIZE = 1_000_000;
+    private static final int SLEEP_MILLIS = 1_000;
     private static final ExecutorService executor = Executors.newFixedThreadPool(10);
-    private static final Object monitor = new Object();
     private final Set<String> strings = new SetSemaphore<>();
     private final List<String> stringList = new ListWithLock<>();
 
@@ -54,5 +56,45 @@ public class Task3Test {
                 // Nothing to do.. Alright!
             }
         }));
+    }
+
+    @Test
+    void addAllItemsInSet() {
+        executor.execute(() -> {
+            for (int i = 0; i < SIZE / 2; ++i) {
+                strings.add(i + "");
+            }
+        });
+        executor.execute(() -> {
+            for (int i = 0; i < SIZE / 2; ++i) {
+                strings.add(SIZE / 2 + i + "");
+            }
+        });
+        try {
+            TimeUnit.MILLISECONDS.sleep(SLEEP_MILLIS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertEquals(SIZE, strings.size());
+    }
+
+    @Test
+    void addAllItemsInList() {
+        executor.execute(() -> {
+            for (int i = 0; i < SIZE / 2; ++i) {
+                stringList.add(i + "");
+            }
+        });
+        executor.execute(() -> {
+            for (int i = 0; i < SIZE / 2; ++i) {
+                stringList.add(SIZE / 2 + i + "");
+            }
+        });
+        try {
+            TimeUnit.MILLISECONDS.sleep(SLEEP_MILLIS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertEquals(SIZE, stringList.size());
     }
 }
